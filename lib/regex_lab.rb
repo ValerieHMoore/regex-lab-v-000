8115bbef97a98131e3676f0d585436a0d26cd1ea
Valerie Moore <periwinkle17@outlook.com>
2018-06-08 13:39:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(word_string)
+  word_string.scan(/\bun\w*ing\b/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(word_string)
+  word_string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/\b\w[A-Z]\w*\[?!."]\b/)
+  #\A\w[A-Z]\w[a-z]\[?!."]\z
 end
 
 def valid_phone_number?(phone)
